7dea0708d7646fa181046483ccccdf0795d576c3
MeeM6336 <anthony.nguyen6336@gmail.com>
2025-03-26 14:42:14 -0500

    Began implementing frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
